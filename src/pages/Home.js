f5d6fbb2ff84bf5358c05ec7f6da83ae4f106065
kimdongouk <kidyac1@gmail.com>
2023-06-21 18:17:46 +0900

    6-21 수업 마지막진도

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,17 @@
+import Editor from "../component/Editor";
+
 const Home = () => {
-  return <div>Home 페이지입니다.</div>;
+  return (
+    <div>
+      <Editor 
+        initData={{
+          date: new Date().getTime(),
+          emotionId: 1,
+          content: "이전에 작성했던 일기"
+        }}
+        onSubmit={() => {alert('작성 완료 버튼을 클릭했음');}}
+        />
+    </div>
+  );
 };
 export default Home;
